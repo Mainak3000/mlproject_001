bda55ed80d7b675a2b3a07da5c7d2c5cdea9425a
Mainak3000 <ce20b015@iittp.ac.in>
2025-05-04 10:44:44 -0700

    training pipeline added, folder structure & dvc tracking changed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## End to end ML project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Mainak3000/mlproject_001.mlflow \
+MLFLOW_TRACKING_USERNAME=Mainak3000 \
+MLFLOW_TRACKING_PASSWORD=xxxx \
--- src/mlproject_001/components/model_trainer.py
+++ src/mlproject_001/components/model_trainer.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
+from mlflow.models.signature import infer_signature
 import numpy as np
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
@@ -30,6 +31,13 @@ class modelTrainer:
     def __init__(self):
         self.model_tainer_config = modelTrainerConfig()
     
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+
+        return rmse, mae, r2
+
     def initiate_model_trainer(self, train_arr, test_arr):
         try:
             logging.info("Split dv and idv from data")
@@ -51,36 +59,36 @@ class modelTrainer:
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    'splitter':['best','random'],
-                    'max_features':['sqrt','log2'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
                 "Random Forest":{
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    'max_features':['sqrt','log2', None],
-                    'n_estimators': [8,16,32,64,128,256]
+                    # 'max_features':['sqrt','log2', None],
+                    # 'n_estimators': [8,16,32,64,128,256]
                 },
                 "Gradient Boosting":{
                     'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    'criterion':['squared_error', 'friedman_mse'],
-                    'max_features':[None,'sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                    # 'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':[None,'sqrt','log2'],
+                    # 'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                    # 'n_estimators': [8,16,32,64,128,256]
                 },
                 "CatBoosting Regressor":{
                     'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    # 'iterations': [30, 50, 100]
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
-                    'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                    # 'loss':['linear','square','exponential'],
+                    # 'n_estimators': [8,16,32,64,128,256]
                 }
                 
             }
@@ -101,6 +109,42 @@ class modelTrainer:
             best_model = models[best_model_name]
 
             print(f"best model is : {best_model_name} & parameters : {best_model.get_params()}")
+            
+            mlflow.set_registry_uri("https://dagshub.com/Mainak3000/mlproject_001.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            # mlflow
+            with mlflow.start_run():
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_model.get_params())
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2_score", r2)
+
+                # Model registry does not work with file store
+                input_example = X_test[:1]  # one row of test input
+                signature = infer_signature(X_test, best_model.predict(X_test))
+
+                # Log model with signature and input example
+                if tracking_url_type_store != "file":
+                    mlflow.sklearn.log_model(
+                        best_model,
+                        "model",
+                        registered_model_name=best_model_name,
+                        input_example=input_example,
+                        signature=signature
+                    )
+                else:
+                    mlflow.sklearn.log_model(
+                        best_model,
+                        "model",
+                        input_example=input_example,
+                        signature=signature
+                    )
 
             if best_score<0.6:
                 raise CustomException("No best model found")
