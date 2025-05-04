268039f01e1b9c01bdf16f449647ded38fd5e713
Mainak3000 <ce20b015@iittp.ac.in>
2025-05-04 06:08:14 -0700

    model trainer added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.mlproject_001.logger import logging
 from src.mlproject_001.exception import CustomException
 from src.mlproject_001.components.data_ingestion import dataIngestion
 from src.mlproject_001.components.data_transformation import dataTransformationConfig, dataTransformation
-
+from src.mlproject_001.components.model_trainer import modelTrainerConfig, modelTrainer
 
 if __name__=="__main__":
     logging.info("The execution has started")
@@ -13,9 +13,15 @@ if __name__=="__main__":
         data_ingestion = dataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         
+        # data transformation
         data_transformation = dataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path=train_data_path, test_data_path=test_data_path)
-        
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        # model training
+        model_trainer = modelTrainer()
+        model_r2_score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+        print(model_r2_score)
+
     except Exception as e:
         logging.info("Custom Exception raised")
         raise CustomException(e, sys)
--- src/mlproject_001/utils.py
+++ src/mlproject_001/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 from src.mlproject_001.exception import CustomException
 from src.mlproject_001.logger import logging
@@ -11,6 +13,9 @@ import redshift_connector
 
 import pickle
 import joblib
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 load_dotenv()
 
@@ -34,7 +39,7 @@ def read_sql_data():
         logging.info("Connection established: %s", conn_redshift)
         
         df = pd.read_sql("""select * from adf0793.students""", conn_redshift)
-        print(df.head())
+        print("Shape of the data", df.shape)
         return df
     
     except Exception as e:
@@ -47,5 +52,30 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            grid = GridSearchCV(model, param, cv=3)
+            grid.fit(X_train, y_train)
+
+            model.set_params(**grid.best_params_)        
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+
+            test_score = r2_score(y_test, y_pred)
+
+            report[list(models.keys())[i]] = test_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
