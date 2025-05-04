ee3c94393ff9d3f9328ef7efcf57d4dc0d6b4b9a
Mainak3000 <ce20b015@iittp.ac.in>
2025-05-04 12:25:10 -0700

    final changes (hopefully)

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 ## End to end ML project
 
+This is an end-to-end machine learning project
+
+It includes custom logger, custom exception handler, custom data ingestion, data trainer and data predictor through Flask app
+
+Model data is tracked by DVC and Model runs are tracked by MLflow.
+
+### Running instructuctions
+
+ 1. git clone https://github.com/Mainak3000/mlproject_001.git
+ 2. cd mlproject_001
+ 3. run conda create -p project_env python==3.9 -y
+ 4. run pip install -r requirements.txt
+ 5. run python template.py
+ 6. To create train data, test data, final model and data preprocessor, run python src/mlproject_001/pipelines/training_pipeline.py 
+ 7. run python app.py
+
 MLFLOW_TRACKING_URI=https://dagshub.com/Mainak3000/mlproject_001.mlflow \
 MLFLOW_TRACKING_USERNAME=Mainak3000 \
 MLFLOW_TRACKING_PASSWORD=xxxx \
--- template.py
+++ template.py
@@ -7,18 +7,17 @@ logging.basicConfig(level=logging.INFO)
 project_name = "mlproject_001"
 
 list_of_files = [
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/model_trainer.py",
     f"src/{project_name}/components/model_monitoring.py",
-    f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
     f"src/{project_name}/pipelines/prediction_pipeline.py",
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
     f"src/{project_name}/utils.py",
+    "templates/index.html",
+    "templates/home.html",
     "app.py",
     "Dockerfile",
     "requirements.txt",
